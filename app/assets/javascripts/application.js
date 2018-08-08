074fea400d45f46ccd0ae88baafb05e14e834960
Enzo Mathe <enzomathe@gmail.com>
2018-08-08 16:13:45 +0200

    MVP Features done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,21 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=1 rue de Vendee Bouffere France`)
+    .then(response => response.json())
+    .then((data) => {
+      const map = document.getElementById("map");
+      const lat = data.results[0].geometry.location.lat;
+      const lng = data.results[0].geometry.location.lng;
+      map.insertAdjacentHTML("beforeend", `<h4>${lat}</h4>`);
+      map.insertAdjacentHTML("beforeend", `<h4>${lng}</h4>`);
+      const marker = { lat: `${lat}`, lng: `${lng}` };
+      console.log(marker);
+      const map = new GMaps({
+        el: '#map',
+        lat,
+        lng,
+        zoom: 14 });
+      map.addMarkers([marker]);
+    });
